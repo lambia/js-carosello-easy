35aeecc10f2f38f6421ce88b9ecf5d98f41851c5
Luca Lambiase <luca.lambia@gmail.com>
2023-11-03 14:10:13 +0100

    refactor: replace spaces with tabs

--- js/carosello.js
+++ js/carosello.js
@@ -1,21 +1,21 @@
 //al caricamento della pagina voglio inserire delle immagini
 let percorsi = [
-    "https://picsum.photos/id/101/900/400", //0
-    "https://picsum.photos/id/102/900/400", //1
-    "https://picsum.photos/id/103/900/400", //2
-    "https://picsum.photos/id/104/900/400", //3
-    "https://picsum.photos/id/200/400/800"  //4
+	"https://picsum.photos/id/101/900/400", //0
+	"https://picsum.photos/id/102/900/400", //1
+	"https://picsum.photos/id/103/900/400", //2
+	"https://picsum.photos/id/104/900/400", //3
+	"https://picsum.photos/id/200/400/800"  //4
 ];
 
 for (let i = 0; i < percorsi.length; i++) {
 
-    immagine = `<img class="slide" src="${percorsi[i]}"></img>`;
-    document.getElementById("image-wrapper").innerHTML += immagine;
+	immagine = `<img class="slide" src="${percorsi[i]}"></img>`;
+	document.getElementById("image-wrapper").innerHTML += immagine;
 }
 
 // let x = 0;
 // while (x < percorsi.length) {
-    
+
 //     immagine = `<img class="slide" src="${percorsi[x]}"></img>`;
 //     document.getElementById("image-wrapper").innerHTML += immagine;
 //     x++;
@@ -32,20 +32,20 @@ immagini[imgAttuale].classList.add("visible");
 // gestire evento di click sulle freccette
 // al click, cambiare l'immagine visibile
 document.getElementById("prevBtn").addEventListener("click", function () {
-    console.log("indietro");
+	console.log("indietro");
 });
 
 document.getElementById("nextBtn").addEventListener("click", function () {
 
-    if (imgAttuale < ultimaImg) {
-        //rimuovo la visibilità sull'immagine attuale
-        immagini[imgAttuale].classList.remove("visible");
+	if (imgAttuale < ultimaImg) {
+		//rimuovo la visibilità sull'immagine attuale
+		immagini[imgAttuale].classList.remove("visible");
 
-        //aggiorno il contatore dell'immagine attuale
-        imgAttuale++;
+		//aggiorno il contatore dell'immagine attuale
+		imgAttuale++;
 
-        //aggiungo la visibilità sulla prossima immagine
-        immagini[imgAttuale].classList.add("visible");
-    }
+		//aggiungo la visibilità sulla prossima immagine
+		immagini[imgAttuale].classList.add("visible");
+	}
 
 });
